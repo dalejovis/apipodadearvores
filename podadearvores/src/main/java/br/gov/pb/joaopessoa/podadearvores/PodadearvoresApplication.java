daeb3688a5c3cf51d92d8c594e1bbc472f5596b9
dalejovis <contato.jovis@gmail.com>
2022-11-12 15:06:54 -0300

    first commit

---
+++ podadearvores/src/main/java/br/gov/pb/joaopessoa/podadearvores/PodadearvoresApplication.java
@@ -0,0 +1,13 @@
+package br.gov.pb.joaopessoa.podadearvores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PodadearvoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PodadearvoresApplication.class, args);
+	}
+
+}
---
+++ podadearvores/src/test/java/br/gov/pb/joaopessoa/podadearvores/PodadearvoresApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.pb.joaopessoa.podadearvores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PodadearvoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
